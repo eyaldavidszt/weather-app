0c0a660db71f0b00687300d0a6e429617bcf0ab5
Hellohellohello12313 <66707792+hellohellohello12321@users.noreply.github.com>
2023-07-28 05:35:34 +0300

    Add weather icon static assets

--- src/dynamicdom.js
+++ src/dynamicdom.js
@@ -1,1 +1,15 @@
 // functions that create dynamically created dom elements, like weather cards.
+
+function makeCard(item) {
+  console.log(item);
+  for (let property in item) {
+    console.log(`${property}: ${item[property]}`);
+  }
+}
+function makeCards(array) {
+  for (let i = 0; i < array.length; i++) {
+    makeCard(array[i]);
+  }
+}
+
+export { makeCards };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./assets/style.css";
 import { getLocationRelevant } from "./api";
+import { makeCards } from "./dynamicdom";
 
 const searchBtn = document.querySelector("[data-search-btn]");
 const inputLocationEl = document.querySelector("[data-location-input]");
@@ -7,5 +8,5 @@ const inputLocationEl = document.querySelector("[data-location-input]");
 searchBtn.addEventListener("click", async function (event) {
   event.preventDefault();
   const array = await getLocationRelevant(inputLocationEl.value);
-  console.log(array);
+  makeCards(array);
 });
